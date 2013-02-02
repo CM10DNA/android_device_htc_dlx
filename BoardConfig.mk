982760caf853cba6f13f6f52fc925898a3babd0a
David Hays <Dhays90@gmail.com>
2013-02-01 20:38:29 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,7 @@ BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 #BOARD_WLAN_DEVICE           := bcm4334
 WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcmdhd.ko"
 WIFI_DRIVER_FW_STA_PATH     := "/system/vendor/firmware/fw_bcm4334.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcm4334_p2p.bin"
 WIFI_DRIVER_FW_AP_PATH      := "/system/vendor/firmware/fw_bcm4334_apsta.bin"
 
 BOARD_USES_SECURE_SERVICES := true

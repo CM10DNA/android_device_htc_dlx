9e8279379caa2ffe598e609923f7ba99388f2221
David Hays <Dhays90@gmail.com>
2013-02-26 08:14:00 -0600

    Clean up...

--- cm.mk
+++ cm.mk
@@ -14,8 +14,8 @@ $(call inherit-product, device/htc/dlx/device_dlx.mk)
 PRODUCT_DEVICE := dlx
 PRODUCT_NAME := cm_dlx 
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := dlx
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := DLX
+PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name etc.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_dlx BUILD_FINGERPRINT=verizon_wwe/dlx/dlx:4.1.1/JRO03C/123160.4:user/release-keys PRIVATE_BUILD_DESC="1.15.605.4 CL123160 release-keys" BUILD_NUMBER=123160

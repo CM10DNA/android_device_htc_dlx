a89c02713b46fd240b3a0bf653a69a42e0ad6f20
Chad Froebel <chad0989@rochester.rr.com>
2013-04-17 21:56:19 -0400

    Add libloc_api_v02 from released mako libs. This is a temporary GPS fix until we can come up with a GPS HAL that will work with our version.

--- device_dlx.mk
+++ device_dlx.mk
@@ -119,6 +119,9 @@ PRODUCT_PACKAGES += \
         libgps.utils \
         gps.msm8960
 
+PRODUCT_COPY_FILES += \
+    device/htc/dlx/prebuilt/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so
+
 # NFC
 PRODUCT_PACKAGES += \
     libnfc \

5fd2b4766f7deb35b640789e41e510b5ac67eb1e
Chad Froebel <chad0989@rochester.rr.com>
2013-07-30 17:54:16 -0400

    dlx: Include low latency audio permissions
    Change-Id: I0d3bf9bfcd94d29060ccacf0ef2c2fe8e4f33596

--- full_dlx.mk
+++ full_dlx.mk
@@ -54,6 +54,7 @@ else
     NFCEE_ACCESS_PATH := device/htc/dlx/configs/nfcee_access_debug.xml
 endif
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml \
     frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \

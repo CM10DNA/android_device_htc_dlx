05fb61e5abeca8ed2e84dffff63f5c3f69899dea
Chad Froebel <chad0989@rochester.rr.com>
2013-08-02 20:35:45 -0400

    dlx: Fix bluetooth calling * Enable I2SPCM slave mode for SCO * Add ACDBID for proxy device * Update BT parameters * Lower Voice Earpiece gain
    Change-Id: I0d28764fd5d609d2a4bafb94595a3310b77e1646

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -1,4 +1,6 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME "HTC Droid DNA"
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define I2SPCM_SLAVE_BRCM TRUE
 #endif
